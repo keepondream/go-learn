b2794b308864bd4ee764062ecbe3137f094c5399
wangsx <402314889@qq.com>
2019-10-22 13:26:36 +0800

    create

---
+++ src/atestgithub/test1/atestg_test.go
@@ -0,0 +1,15 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-16 13:40:57
+ * @LastEditTime: 2019-10-16 13:40:57
+ * @LastEditors: your name
+ */
+package test1_test
+
+import "testing"
+
+func TestParams(t *testing.T) {
+	t.Log("ccc")
+	t.Log("aaa")
+}
